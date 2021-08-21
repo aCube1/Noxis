5702ff7e88b48b9f2735d475dc8359efe3b7c073
aCube <player559001@gmail.com>
2021-08-21 13:28:27 -0300

    Remove useless struct in utils.h

--- include/core/init.h
+++ include/core/init.h
@@ -8,14 +8,6 @@
 
 #include "utils.h"
 
-struct {
-	SDL_Window **window;
-	SDL_Renderer **renderer;
-	SDL_Rect window_rect;
-
-	const char *title;
-} NOX_Config_SDL2_t;
-
 Uint8 NOX_InitSDL2(SDL_Window **window, SDL_Renderer **renderer, SDL_Rect window_rect, const char *title);
 
 #endif /* _INIT_H_ */
